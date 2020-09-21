44d60c9751aac7e1c61cf9d6b194a0e91069e7ce
BagchiMB <mbagchi6884@gmail.com>
2020-09-21 18:59:13 +0530

    Updated Readme :tada:

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `enter-title`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@Mihir`,
   },
   pathPrefix: `/case-study/enter-title/`,
   plugins: [
